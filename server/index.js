67bfe13fd4a9567e24fcedeff293b93f900130cc
Alexandre Zagame <zagamealexandre@gmail.com>
2021-01-25 16:52:01 +0100

    fixing cors

--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-// app.use(cors());
+app.use(cors());
 const corsOptions = {
-  origin: 'http://localhost:3000/',
+  origin: 'http://localhost:3000',
 };
 app.use(cors(corsOptions));
 app.use('/posts', postRoutes);
